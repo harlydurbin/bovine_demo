17e48fa2eada1b14263cc06525771de7b7f31d65
harlydurbin <hjdzpd@mail.missouri.edu>
2020-08-10 14:42:21 -0500

    finishing up smartpca results, start re-running faststructure

--- source_functions/faststructure.bovine_demo.snakefile
+++ source_functions/faststructure.bovine_demo.snakefile
@@ -1,4 +1,4 @@
-# snakemake -s source_functions/faststructure.bovine_demo.snakefile -j 1000 --rerun-incomplete --keep-going --latency-wait 30 --use-conda --resources load=100 --config --cluster-config source_functions/cluster/faststructure.cluster.json --cluster "sbatch -p {cluster.p} -o {cluster.o} --account {cluster.account} -t {cluster.t} -c {cluster.c} --mem {cluster.mem} --account {cluster.account} --mail-user {cluster.mail-user} --mail-type {cluster.mail-type} --qos {cluster.qos}" -p &> log/snakemake_log/faststructure/200805.faststructure.log
+# snakemake -s source_functions/faststructure.bovine_demo.snakefile -j 1000 --rerun-incomplete --keep-going --latency-wait 30 --use-conda --resources load=100 --config --cluster-config source_functions/cluster/faststructure.cluster.json --cluster "sbatch -p {cluster.p} -o {cluster.o} --account {cluster.account} -t {cluster.t} -c {cluster.c} --mem {cluster.mem} --account {cluster.account} --mail-user {cluster.mail-user} --mail-type {cluster.mail-type} --qos {cluster.qos}" -p &> log/snakemake_log/faststructure/200810.faststructure.log
 
 include: "plink_qc.snakefile"
 
@@ -19,9 +19,9 @@ rule faststructure_all:
 
 rule structure:
 	input:
-		merged_bed = "data/derived_data/plink_qc/thin_variants/merge_thinned.{dataset}.{thin_p}.bed",
-		merged_bim = "data/derived_data/plink_qc/thin_variants/merge_thinned.{dataset}.{thin_p}.bim",
-		merged_fam = "data/derived_data/plink_qc/thin_variants/merge_thinned.{dataset}.{thin_p}.fam"
+		merged_bed = "data/derived_data/plink_qc/thin_variants/{dataset}.{thin_p}/merge_thinned.{dataset}.{thin_p}.bed",
+		merged_bim = "data/derived_data/plink_qc/thin_variants/{dataset}.{thin_p}/merge_thinned.{dataset}.{thin_p}.bim",
+		merged_fam = "data/derived_data/plink_qc/thin_variants/{dataset}.{thin_p}/merge_thinned.{dataset}.{thin_p}.fam"
 	# Snakemake looks for envs/ relative to the snakefile
 	conda:
 		"envs/faststructure.yaml"
--- source_functions/plot_pca.R
+++ source_functions/plot_pca.R
@@ -4,7 +4,8 @@ plot_pca <-
            pc1,
            pc2,
            color_var,
-           shape_var = NULL) {
+           shape_var = NULL,
+           title_string = NULL) {
     col1 <- rlang::sym(str_c("PC", as.character(pc1)))
     col2 <- rlang::sym(str_c("PC", as.character(pc2)))
     
@@ -32,7 +33,8 @@ plot_pca <-
       set_names(c("international_id", map_chr(1:10, ~ str_c("PC", .x)))) %>%
       mutate(international_id = str_remove(international_id, ":[[:graph:]]+")) %>%
       left_join(
-        sample_metadata %>%
+        read_csv(here::here("data/derived_data/metadata/bovine_demo.sample_metadata.csv")) %>% 
+          mutate(international_id = if_else(international_id == "CPC98_Bprimigenius_EnglandDerbyshire_5936", "ancient_derbyshire", international_id)) %>%
           select(international_id, population, region, continent, species)
       ) %>%
       mutate(
@@ -70,8 +72,7 @@ plot_pca <-
       labs(
         x = str_c("PC ", pc1, ": ", scales::percent(val[pc1] / sum(val))),
         y = str_c("PC ", pc2, ": ", scales::percent(val[pc2] / sum(val))),
-        color = stringr::str_to_sentence(rlang::quo_name(rlang::enquo(color_var))),
-        title = glue::glue("{dataset_thinning}: PC {pc1} vs. PC {pc2}")
+        color = stringr::str_to_sentence(rlang::quo_name(rlang::enquo(color_var)))
       )
     
     plot <-
@@ -82,8 +83,69 @@ plot_pca <-
           labs(shape = stringr::str_to_sentence(rlang::quo_name(shape_var)))
       }
     
+    plot <-
+      if(is.null(title_string)){
+        plot +
+          labs(title = glue::glue("{dataset_thinning}: PC {pc1} vs. PC {pc2}"))
+      } else{
+        plot +
+          labs(title = glue::glue("{title_string}: PC {pc1} vs. PC {pc2}"))
+      }
+    
     return(plot)
   }
+
+ggscree <- function(dataset_thinning, title_string = NULL) {
+  
+  path <-
+    glue::glue(
+      "data/derived_data/smartpca/{dataset_thinning}/smartpca.{dataset_thinning}.evec"
+    )
+  
+  val <-
+    read_lines(here::here(path), n_max = 1) %>%
+    str_remove("#eigvals:") %>%
+    str_squish() %>%
+    str_split(pattern = "[[:space:]]") %>%
+    flatten_chr() %>%
+    as_tibble() %>%
+    mutate(
+      PC = as.character(glue::glue("PC{row_number()}")),
+      PC = forcats::fct_inorder(PC),
+      value = as.double(value),
+      varexp = round((value / sum(value))*100, digits = 2)
+    )
+  
+  plot <-
+    val %>%
+    ggplot2::ggplot() +
+    ggplot2::geom_col(mapping = ggplot2::aes(x = PC, y = varexp), stat = "identity") +
+    #scale_y_continuous(limits = c(0, 15)) +
+    geom_text(
+      aes(
+        x = PC,
+        y = varexp,
+        label = str_c(varexp, "%")
+      ),
+      position = position_dodge(width = 0.9),
+      vjust = -0.25
+    ) +
+    theme_classic() +
+    labs(x = NULL,
+         y = "% Variation explained") 
+  
+  plot <-
+    if(is.null(title_string)){
+      plot +
+        labs(title = dataset_thinning)
+    } else{
+      plot +
+        labs(title = title_string)
+    }
+  
+  return(plot)
+  
+}
 
 read_vec <- 
   
